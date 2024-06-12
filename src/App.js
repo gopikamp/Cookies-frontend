bccd42952fce62b857690c89c1666ea4dbe15748
GOPIKA MP <gopikaramesh360@gmail.com>
2024-06-13 00:12:12 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Addcookie from './components/Addcookie';
+import Searchcookie from './components/Searchcookie';
 
 function App() {
   return (
     <div>
       <Addcookie/>
+      <Searchcookie/>
     </div>
   );
 }
